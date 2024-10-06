a4026e9d8264ac9c281597bd3e48da084aeaa59e
shivram <shivramm@gmail.com>
2024-10-06 21:26:17 +0530

    Commit from NewTest

---
+++ src/test/java/testpack/NewTestFile.java
@@ -0,0 +1,11 @@
+package testpack;
+
+public class NewTestFile {
+
+	public static void main(String[] args)
+	
+	{
+		System.out.println("This is a print from NewTestFile");
+	}
+
+}
--- src/test/java/testpack/TestPrint.java
+++ src/test/java/testpack/TestPrint.java
@@ -7,11 +7,9 @@ public class TestPrint {
 	{
 	System.out.println("This is Print Statement 1");
 	System.out.println("This is Print Statement 2");
-<<<<<<< HEAD
+
 	System.out.println("This is Print Statement 5");
-=======
 	System.out.println("This is Print Statement 3");
->>>>>>> 222180b2ff3b2e9aa60a3a78c858ce67fc3f9934
 	}
 
 }
